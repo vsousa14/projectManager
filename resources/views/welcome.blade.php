72bd6db97b9f5d661ea137b7eb141e6171c67261
vsousa14 <vasco.sousa14@hotmail.com>
2025-07-25 15:53:02 +0100

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome to Project Manager</div>
+
+                <div class="card-body">
+                    This is a simple project management application built with Laravel.
+                </div> 
+            </div>  
+        </div>
+    </div>
+</div>
+@endsection
